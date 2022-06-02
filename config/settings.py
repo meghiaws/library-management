88678dd5765b682c0b8cd1c98f3baf2e16950d7f
Mahdi Ghiasi <mahdiw.ghiasi@gmail.com>
2022-06-02 23:57:01 +0430

    Remove `DEFAULT_FILTER_BACKENDS` from settings

--- config/settings.py
+++ config/settings.py
@@ -33,7 +33,6 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
-    "DEFAULT_FILTER_BACKENDS": ("django_filters.rest_framework.DjangoFilterBackend",),
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
 
