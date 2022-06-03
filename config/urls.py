2714fa32c6b0708fb8769bfd4510616bba75466d
Mahdi Ghiasi <mahdiw.ghiasi@gmail.com>
2022-06-04 04:07:21 +0430

    Add auth endpoints to project `urls.py`

--- config/urls.py
+++ config/urls.py
@@ -7,10 +7,12 @@ from drf_spectacular.views import (
 )
 
 urlpatterns = [
-    path("", include("library.urls")),
+    path("admin/", admin.site.urls),
+    path("library/", include("library.urls")),
+    path("auth/", include('djoser.urls')),
+    path("auth/", include('djoser.urls.jwt')),
     path("__debug__/", include("debug_toolbar.urls")),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path("api/docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger"),
     path("api/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
-    path("admin/", admin.site.urls),
 ]
