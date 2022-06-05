8240bd0c2f219b34966542e8b54fc078a65a19b2
Mahdi Ghiasi <mahdiw.ghiasi@gmail.com>
2022-06-05 18:09:16 +0430

    Remove `get_queryset()` from `MemberViewset`

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,10 @@
 from django.contrib.auth import get_user_model
 
 from rest_framework.viewsets import ModelViewSet, GenericViewSet
+from rest_framework.permissions import IsAdminUser
 from rest_framework.generics import mixins
 
-from .models import Member
+from .models import Librarian, Member
 from .permissions import IsAdminOrLibrarian
 from .serializers import (
     MemberSerializer,
@@ -18,10 +19,6 @@ class MemberViewset(ModelViewSet):
     queryset = Member.objects.select_related("user").all()
     permission_classes = [IsAdminOrLibrarian]
 
-    def get_queryset(self):
-        if self.action == "create":
-            return User.objects.all()
-        return Member.objects.select_related("user").all()
 
     def get_serializer_class(self):
         if self.action == "create":
