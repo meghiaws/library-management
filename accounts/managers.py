5ef4a029133c103bb35fd92738454cfd08f45587
Mahdi Ghiasi <mahdiw.ghiasi@gmail.com>
2022-06-05 18:36:23 +0430

    Add Model Manager for `Librarian` model

--- accounts/managers.py
+++ accounts/managers.py
@@ -16,3 +16,17 @@ class MemberManager(Manager):
         )
         membership_code = create_random_8_digits_code()
         return self.create(membership_code=membership_code, user=user)
+
+
+class LibrarianManager(Manager):
+    def create_librarian(self, username, password, email, first_name, last_name):
+        User = get_user_model()
+        user = User.objects.create_user(
+            username=username,
+            password=password,
+            email=email,
+            first_name=first_name,
+            last_name=last_name,
+        )
+        staff_code = create_random_8_digits_code()
+        return self.create(staff_code=staff_code, user=user)
--- accounts/models.py
+++ accounts/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 from django.conf import settings
 
-from .managers import MemberManager
+from .managers import MemberManager, LibrarianManager
 
 
 class Librarian(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     staff_code = models.CharField(max_length=8)
+
+    objects = LibrarianManager()
 
     def __str__(self):
         return f"Librarian: {self.user.username}"
@@ -17,6 +19,6 @@ class Member(models.Model):
     membership_code = models.CharField(max_length=8)
 
     objects = MemberManager()
-    
+
     def __str__(self):
         return f"Member: {self.user.username}"
