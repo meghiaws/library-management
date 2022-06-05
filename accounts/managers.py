51e29570b68f48fd6043edf05191b8ffb225f1c2
Mahdi Ghiasi <mahdiw.ghiasi@gmail.com>
2022-06-05 17:49:22 +0430

    Add manager for `Member` model

---
+++ accounts/managers.py
@@ -0,0 +1,18 @@
+from django.db.models.manager import Manager
+from django.contrib.auth import get_user_model
+
+from .utils import create_random_8_digits_code
+
+
+class MemberManager(Manager):
+    def create_member(self, username, password, email, first_name, last_name):
+        User = get_user_model()
+        user = User.objects.create_user(
+            username=username,
+            password=password,
+            email=email,
+            first_name=first_name,
+            last_name=last_name,
+        )
+        membership_code = create_random_8_digits_code()
+        return self.create(membership_code=membership_code, user=user)
