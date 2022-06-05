8545762fa1ed2efd3ac60cc96a39c30b9421212f
Mahdi Ghiasi <mahdiw.ghiasi@gmail.com>
2022-06-05 17:28:29 +0430

    Remove signals for creating profiles for user

--- library/apps.py
+++ library/apps.py
@@ -4,6 +4,4 @@ from django.apps import AppConfig
 class LibraryConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'library'
-
-    def ready(self):
-        import library.signals.handlers
+    
