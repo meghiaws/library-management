d7bb553f5d50d34f24e5a706d62da7eb67a440f0
Mahdi Ghiasi <mahdiw.ghiasi@gmail.com>
2022-06-06 05:13:09 +0430

    Fix relation between `BorrowedBook` to `Member`

--- borrowing/models.py
+++ borrowing/models.py
@@ -5,7 +5,7 @@ from django.core.validators import MinValueValidator
 
 class BorrowedBook(models.Model):
     book_item = models.OneToOneField("library.BookItem", on_delete=models.CASCADE)
-    borrower = models.OneToOneField("accounts.Member", on_delete=models.CASCADE)
+    borrower = models.ForeignKey("accounts.Member", on_delete=models.CASCADE)
     borrowed_date = models.DateField(auto_now_add=True)
     due_date = models.DateField(validators=[MinValueValidator(date.today)])
 
