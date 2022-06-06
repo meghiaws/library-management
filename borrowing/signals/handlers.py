9ae8e0e4fa52d5bf3a1b927a737837c12647d9f3
Mahdi Ghiasi <mahdiw.ghiasi@gmail.com>
2022-06-07 01:44:01 +0430

    Check availability of a bookitem in signal

--- borrowing/signals/handlers.py
+++ borrowing/signals/handlers.py
@@ -10,7 +10,8 @@ from ..models import BorrowedBook
 def update_status_of_book_item_to_borrowed(sender, instance, created, **kwargs):
     if created:
         book_item = instance.book_item
-        book_item.change_status(to=BookItem.STATUS_BORROWED)
+        if book_item.is_available():
+            book_item.change_status(to=BookItem.STATUS_BORROWED)
 
 
 @receiver(post_delete, sender=BorrowedBook)
