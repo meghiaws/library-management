4398612fc663dd62a65f60e83cf6164611e2fc8e
Mahdi Ghiasi <mahdiw.ghiasi@gmail.com>
2022-06-06 19:53:56 +0430

    Use signals for change the status of BookItems

--- borrowing/apps.py
+++ borrowing/apps.py
@@ -1,6 +1,10 @@
+from unittest import signals
 from django.apps import AppConfig
 
 
 class BorrowingConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'borrowing'
+
+    def ready(self) -> None:
+        import borrowing.signals.handlers
---
+++ borrowing/signals/handlers.py
@@ -0,0 +1,19 @@
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+
+from library.models import BookItem
+
+from ..models import BorrowedBook
+
+
+@receiver(post_save, sender=BorrowedBook)
+def update_status_of_book_item_to_borrowed(sender, instance, created, **kwargs):
+    if created:
+        book_item = instance.book_item
+        book_item.change_status(to=BookItem.STATUS_BORROWED)
+
+
+@receiver(post_delete, sender=BorrowedBook)
+def update_status_of_book_item_to_available(sender, instance, **kwargs):
+    book_item = instance.book_item
+    book_item.change_status(to=BookItem.STATUS_AVAILABLE)
