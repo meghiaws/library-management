6dbfd27c21dbef8fc31bb2afba6e452ff0d39378
Mahdi Ghiasi <mahdiw.ghiasi@gmail.com>
2022-06-06 19:58:46 +0430

    Add reservation app urls to project URLconf

--- config/urls.py
+++ config/urls.py
@@ -12,6 +12,7 @@ urlpatterns = [
     path("library/", include("library.urls")),
     path("accounts/", include("accounts.urls")),
     path("borrowing/", include("borrowing.urls")),
+    path("reservation/", include("reservation.urls")),
     path("__debug__/", include("debug_toolbar.urls")),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path("api/docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger"),
