31b4c55c4c6d80fe095182921945c71318ead540
Mahdi Ghiasi <mahdiw.ghiasi@gmail.com>
2022-06-07 19:05:36 +0430

    Add and configure celery with redis

---
+++ config/celery.py
@@ -0,0 +1,9 @@
+
+import os
+from celery import Celery
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+
+celery = Celery("library-management")
+celery.config_from_object("django.conf:settings", namespace="CELERY")
+celery.autodiscover_tasks()
--- config/settings.py
+++ config/settings.py
@@ -140,9 +140,13 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = "static/"
+
 STATIC_ROOT = BASE_DIR / "static"
 
 MEDIA_URL = "/media/"
+
 MEDIA_ROOT = BASE_DIR / "media"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CELERY_BROKER_URL = "redis://localhost:6379/1"
