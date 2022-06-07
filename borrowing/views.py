1b086753e3dffa17b5e51ab0ca93764365d9db7a
Mahdi Ghiasi <mahdiw.ghiasi@gmail.com>
2022-06-08 00:31:54 +0430

    Restrict APIs to admins and librarians

--- borrowing/views.py
+++ borrowing/views.py
@@ -1,6 +1,8 @@
 from rest_framework.viewsets import GenericViewSet
 from rest_framework import mixins
 
+from accounts.permissions import IsAdminOrLibrarian
+from library.models import BookItem
 from .models import BorrowedBook
 from .serializers import BorrowedBookSerializer, BorrowedBookCreateSerializer
 
@@ -13,6 +15,7 @@ class BorrowedBookViewset(
     GenericViewSet,
 ):
     queryset = BorrowedBook.objects.select_related("book_item").all()
+    permission_classes = [IsAdminOrLibrarian]
 
     def get_serializer_class(self):
         if self.action in ("create"):
--- reservation/views.py
+++ reservation/views.py
@@ -1,6 +1,8 @@
 from rest_framework.viewsets import GenericViewSet
 from rest_framework import mixins
 
+from accounts.permissions import IsAdminOrLibrarian
+
 from .models import ReservedBook
 from .serializers import ReservedBookSerializer, ReservedBookCreateSerializer
 
@@ -13,6 +15,7 @@ class ReservedBookViewset(
     GenericViewSet,
 ):
     queryset = ReservedBook.objects.select_related("book_item").all()
+    permission_classes = [IsAdminOrLibrarian]
 
     def get_serializer_class(self):
         if self.action in ("create"):
