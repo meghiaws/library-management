8c600241da718e1025198e8a147048271da8f14d
Mahdi Ghiasi <mahdiw.ghiasi@gmail.com>
2022-06-08 00:13:14 +0430

    Add two helper method for `BorrowedBook`

--- borrowing/models.py
+++ borrowing/models.py
@@ -9,6 +9,16 @@ class BorrowedBook(models.Model):
     borrowed_date = models.DateField(auto_now_add=True)
     due_date = models.DateField(validators=[MinValueValidator(date.today)])
 
+    def is_due_date_past(self):
+        if self.due_date < date.today():
+            return True
+        return False
+
+    def how_many_days_past_from_due_date(self):
+        td = date.today() - self.due_date
+        if td.days >= 0:
+            return td.days
+
     def __str__(self):
         return (
             f"{self.book_item.book.title} borrowed from {self.borrower.user.username}"
