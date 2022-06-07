a70cdf7e0e3dbdf292c7b2afcd72dff962c0abe1
Mahdi Ghiasi <mahdiw.ghiasi@gmail.com>
2022-06-08 00:32:14 +0430

    Add `Fine` model

--- fines/models.py
+++ fines/models.py
@@ -1,3 +1,18 @@
+from decimal import Decimal
 from django.db import models
 
-# Create your models here.
+
+class Fine(models.Model):
+    member = models.ForeignKey("accounts.Member", on_delete=models.CASCADE)
+    borrowed_book = models.OneToOneField(
+        "borrowing.BorrowedBook", on_delete=models.CASCADE, unique=True
+    )
+    amount = models.DecimalField(max_digits=4, decimal_places=2, default=0.00)
+
+    @staticmethod
+    def calculate_fine(borrowed_book) -> Decimal:
+        past_days_count = borrowed_book.how_many_days_past_from_due_date()
+        return Decimal(past_days_count * 5.00)
+
+    def __str__(self) -> str:
+        return f"Fine: {self.amount} for {self.member}"
