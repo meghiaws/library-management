fd6d2842e11670a716364fc10ca0c3cb917b4ed2
Mahdi Ghiasi <mahdiw.ghiasi@gmail.com>
2022-06-08 19:47:49 +0430

    Seprate INSTALLED_APPS

--- config/settings/local.py
+++ config/settings/local.py
@@ -10,6 +10,8 @@ SECRET_KEY = env.str("SECRET_KEY")
 
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
 
+INSTALLED_APPS += ["debug_toolbar"]
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
